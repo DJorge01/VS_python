46a1116b7fd48460ff1f678b17b296450420d573
DJorge01 <perezmarionjorge@gmail.com>
2025-01-16 17:08:42 -0500

    Update main.py

--- my-desktop-app/src/main.py
+++ my-desktop-app/src/main.py
@@ -2,6 +2,8 @@ from PyQt5.QtWidgets import QApplication
 from ui.main_window import MainWindow
 import sys
 # un comentario no esta demas
+#otro comentario no esta demas
+
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
